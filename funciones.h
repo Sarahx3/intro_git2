53cdfe77740bd9a29fd9e829d730b0fa64dcb9d6
Aide Sarahi <aideflores5874@alumnos.udg.mx>
2020-09-27 10:23:28 -0500

    agregada la condicion a resta()

--- funciones.h
+++ funciones.h
@@ -2,11 +2,16 @@
 #define FUNCIONES_H
 
 void suma(int a, int b){
+    
     return a+b;
 
 }
 
 void resta(int a, int b){
+    if (a<0)
+    {
+        return 0;
+    }
     return a-b;
 }
 #endif //funciones_h
